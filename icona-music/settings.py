f91f5188e22cbfdc895c93f704754a2ac698f893
davidwaga <davidwaga75@gmail.com>
2021-07-17 03:29:09 +0300

    [fixing] whitenoise error

--- icona-music/settings.py
+++ icona-music/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f9*3q=t1w^47((hpnt#p!c478$tocv)3r3k12ufw^9*rm53ift'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['icona-music.herokuapp.com']
 
@@ -151,5 +151,5 @@ STATICFILES_DIRS = (
 )
 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
