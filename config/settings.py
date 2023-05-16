518dafe1309bbee5c1fd0d3c51aaeb818b8438b8
davidwaga <davidwaga75@gmail.com>
2023-05-16 22:40:59 +0300

    [update] libraries

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'icona_music.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
---
+++ readme.md
@@ -0,0 +1,2 @@
+# icona music is a simple application that helps music producers and musicians upload their music without middle 
+##
